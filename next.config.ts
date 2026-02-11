aa06a02a2af9b4cd36d0ee0a70001cb52f0c4635
loueysioua <sioua.louey@gmail.com>
2026-02-11 19:35:44 +0100

    fix: unused file

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // Similarly, ignore ESLint errors during builds
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   // Since you are deploying to GitHub Pages, you likely need this too:
   output: "export",
 };
